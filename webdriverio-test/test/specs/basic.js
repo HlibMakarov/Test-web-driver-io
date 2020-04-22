4f9f51a91481a985ef64b8e4074b2425bec3115e
makarovhlib@gmail.com <makarovhlib@gmail.com>
2020-04-22 03:12:36 +0300

    lesson6

--- webdriverio-test/test/specs/basic.js
+++ webdriverio-test/test/specs/basic.js
@@ -7,37 +7,16 @@ describe('webdriver.io page', () => {
         assert.strictEqual(title, 'WebdriverIO · Next-gen browser automation test framework for Node.js')
     })
 
-it('should demonstrate the addValue command', () =>{     // add value
-    browser.url('https://webdriver.io')
-    let input = $('#search_input_react')
-    input.addValue('test')
-    browser.pause(4000);
-    input.addValue(123)
-    browser.pause(4000);
+    it('should demonstrate the getAttribute command', () => {    
+            browser.url('https://webdriver.io');      
+          const input = $('#search_input_react');        
+        let attr = input.getAttribute('title')      
+          console.log("Title attribute is: " + attr) // outputs: "Search"
 
-    value = input.getValue()
-    assert(value === 'test123') //true
-    })
-
-
-it('should demonstrate  setValue command', function () {    // setvalue
-    browser.url('https://webdriver.io');       
-     const elem = $('#search_input_react');        
-     elem.setValue('test123');       
-      browser.pause(2000);    })
-
-
-it('should demonstrate the click command', () => {    //Кликание
-        browser.url('https://webdriver.io');    
-        const blogButton = $('[href="/blog/"]')   
-         browser.pause(2000);    
-         blogButton.click()      
-       browser.pause(2000);      
-       const guideButton = $('.nav-site.nav-site-internal [href="/docs/gettingstarted.html"]')    
-         guideButton.click()    
-         browser.pause(2000);  
-      })
+         input.setValue('test123');      
+      attr = input.getAttribute('value')  
+          console.log("Value attribute is: " + attr) // outputs: test123    })
 })
-
+})
 
 
