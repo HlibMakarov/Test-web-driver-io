a01d9b07f85316ee7ee7fdfa841a8fe9195348ef
makarovhlib@gmail.com <makarovhlib@gmail.com>
2020-04-22 03:03:58 +0300

    base_backup

--- webdriverio-test/test/specs/basic.js
+++ webdriverio-test/test/specs/basic.js
@@ -7,27 +7,27 @@ describe('webdriver.io page', () => {
         assert.strictEqual(title, 'WebdriverIO · Next-gen browser automation test framework for Node.js')
     })
 
-it('should demonstrate the addValue command', () =>{
+it('should demonstrate the addValue command', () =>{     // add value
     browser.url('https://webdriver.io')
     let input = $('#search_input_react')
     input.addValue('test')
     browser.pause(4000);
     input.addValue(123)
-    browser.pause(2000);
+    browser.pause(4000);
 
     value = input.getValue()
     assert(value === 'test123') //true
     })
 
 
-it('should demonstrate  setValue command', function () {   
+it('should demonstrate  setValue command', function () {    // setvalue
     browser.url('https://webdriver.io');       
      const elem = $('#search_input_react');        
      elem.setValue('test123');       
       browser.pause(2000);    })
 
 
-    it('should demonstrate the click command', () => {    
+it('should demonstrate the click command', () => {    //Кликание
         browser.url('https://webdriver.io');    
         const blogButton = $('[href="/blog/"]')   
          browser.pause(2000);    
