22fb92343092a5fdeefa3ac9a7082a322002d71c
makarovhlib@gmail.com <makarovhlib@gmail.com>
2020-04-22 03:22:37 +0300

    lesson8

--- webdriverio-test/test/specs/basic.js
+++ webdriverio-test/test/specs/basic.js
@@ -1,22 +1,20 @@
 const assert = require('assert')
 
 describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-        browser.url('https://webdriver.io')
-        const title = browser.getTitle()
-        assert.strictEqual(title, 'WebdriverIO · Next-gen browser automation test framework for Node.js')
-    })
 
-    it('should demonstrate the getAttribute command', () => {    
-            browser.url('https://webdriver.io');      
-          const input = $('#search_input_react');        
-        let attr = input.getAttribute('title')      
-          console.log("Title attribute is: " + attr) // outputs: "Search"
+    it('should demonstrate the getLocation function', () => {       
+         browser.url('https://webdriver.io');      
+           const logo = $('#search_input_react')    
+             const location = logo.getLocation();    
+             console.log(location);          
+            const xLocation = logo.getLocation('x')    
+              console.log("xLocation" + xLocation);     });
 
-         input.setValue('test123');      
-      attr = input.getAttribute('value')  
-          console.log("Value attribute is: " + attr) // outputs: test123    })
+        it('should demonstrate the getText function', () => {     
+          browser.url('https://webdriver.io');      
+          const blogButton = $('[href="/blog/"]')    
+            console.log("Text for element:" + blogButton.getText());    });
 })
-})
+
 
 
