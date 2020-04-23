df269911c09d16ef50ddd1f182e36406cbbb9a7d
makarovhlib@gmail.com <makarovhlib@gmail.com>
2020-04-23 03:10:51 +0300

    lesson #14 homework

--- webdriverio-test/test/specs/basic.js
+++ webdriverio-test/test/specs/basic.js
@@ -73,14 +73,14 @@
 //     browser.url('https://webdriver.io')
 //     const blogButton = $('[href="/blog/"]') 
 //     let clickable = blogButton.isClickable();
-//     console.log("IS CLICKABLE?: " + clickable);  //outputs: true or false
+//     console.log("IS CLICKABLE?: " + clickable);  //outputs: true! or false
 // })
 
 // it('should detect if an element is displayed', () => {
 //     browser.url('https://webdriver.io')
 //     const blogButton = $('[href="/blog/"]') 
 //     isDisplayed = blogButton.isDisplayed();
-//     console.log("IS DISPLAYED?: " +isDisplayed); //outputs: false
+//     console.log("IS DISPLAYED?: " +isDisplayed); //outputs: true
 // });
 
 // it('should detect if an element is visible', () => {
@@ -91,3 +91,98 @@
 //     let isGitHubDisplaedInViewport = $('#footer [href="https://github.com/webdriverio/webdriverio"]').isDisplayedInViewport();
 //     console.log("isGitHubDisplayedInViewport:" + isGitHubDisplaedInViewport); //outputs: false
 // })
+
+//      Lesson #11. webdriver.io Trainee Level 1(API). Methods is Enabled(), is Focused(), scrolIntoView()
+//      https://youtu.be/js8MIP_Y7r0
+
+// it('should detect if an element is enabled', () => {
+//     browser.url('https://webdriver.io');
+//     const blogButton = $('[href="/blog/"]')
+//     let isEnabled = blogButton.isEnabled();
+//     console.log("isEnabled:" + isEnabled); // output: true
+
+// });
+
+// it('should detect the focus of an element', () => {
+//     browser.url('https://webdriver.io');
+//     const input = $('#search_input_react')
+//     console.log("search.isFocused() before click:" + input.isFocused()); // output: false
+//     browser.pause(2000)
+//     input.click();
+//     console.log("search.isFocused() afte click:" + input.isFocused()); // output: true
+//     browser.pause(2000);
+// })
+// it('should move to element', () => {
+//     browser.url('https://webdriver.io');
+//     const gitHubLink = $('#footer [href="https://github.com/webdriverio/webdriverio"]');
+//     browser.pause(4000);
+//     gitHubLink.scrollIntoView();
+//     browser.pause(4000);
+// })
+
+//      Lesson #12. webdriver.io Trainee Level 1(API). Methods saveScreenshot(), newWindow(), switchWindow()
+//      https://youtu.be/m8gl_-jx6lM
+
+// it('should save a screenshot of the browser view', function() {
+//     browser.url('https://webdriver.io');
+//     const elem = $('#footer [href="https://github.com/webdriverio/webdriverio"]');
+//     elem.saveScreenshot('saveScreenshot.png')
+// });
+// it('should switch to another window', () => {
+//     //open url
+//     browser.url('https://google.com');
+//     //create new window
+//     browser.newWindow('https://webdriver.io')
+//     browser.pause(2000);
+
+//     //switch back via url match
+//     browser.switchWindow('google.com')
+//     browser.pause(2000);
+
+//     //switch back via title match
+//     browser.switchWindow('Next-gen browser automation test framework for Node.js')
+//     browser.pause(2000);
+
+// })
+
+//          Lesson #13. webdriver.io Trainee Level 1(API). Methods getHTML(), waitUntil()
+//          https://youtu.be/Y6Ksa5OMcrU
+
+// it('should wait until', () => {
+//     browser.url('https://webdriver.io');
+//     browser.waitUntil(() => {
+//         return $('[href="/blog/"]').isDisplayed();
+//     }, 5000, 'blog is not displayed');
+// });
+
+// it('should get html for certain elements', () => {
+//     browser.url('https://webdriver.io/docs/api.html');
+//     var outerHTML = $('.siteNavGroupActive').getHTML();
+//     console.log("outerHTML: " + outerHTML);
+    
+//     var innerHTML = $('.siteNavGroupActive').getHTML(false);
+//     console.log("innerHTML: " + innerHTML);
+// })
+
+//              Lesson #14. webdriver.io Trainee Level 1(API). Home task.
+//              https://youtu.be/oY4-xMfsvUQ
+
+it('should get html for certain elements', () => {
+    browser.url('https://webdriver.io/docs/api.html'); //1. Перейти на страничку https://webdriver.io/docs/api.html
+    let HrefText = $('//*[text()="JSONWire protocol"]');
+    let HrefAttr = HrefText.getAttribute('href');
+    
+    browser.newWindow(HrefAttr)  //2. Открыть ссылку //*[text()="JSONWire protocol"] в новой Табе(используем getAttribute() и newWindow()) браузера
+  
+    browser.switchWindow(HrefAttr)  //3. Переключиться на открытую табу
+
+    const SeleniumHQ = $('strong a[href="/SeleniumHQ/selenium/wiki"]') 
+    isDisplayed = SeleniumHQ.isDisplayed();      
+    console.log("IS DISPLAYEDSelenium?: " +isDisplayed); //outputs: true    4. Проверить, что элемент 'strong a[href="/SeleniumHQ/selenium/wiki"]' видимый пользователю (isDisplayed). Результат вывести через consle.log()
+
+    browser.switchWindow('https://webdriver.io/docs/api.html') //5. Переключаемся назад на Табу 'https://webdriver.io/docs/api.html'
+    browser.pause(5000);
+                                        //6. Используя метод waitUntil ждем пока текст, который содержиться в элементе ".postHeaderTitle" (используем метод getText())равен "API Docs"
+
+
+})
